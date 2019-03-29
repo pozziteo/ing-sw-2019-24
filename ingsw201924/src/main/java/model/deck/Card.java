2f9fb42a98b3c36ce94177d4655cd1940c3014c9
sarasacco <sarasaccoo@gmail.com>
2019-03-30 00:03:54 +0100

    create card superclass

---
+++ ingsw201924/src/main/java/model/deck/Card.java
@@ -0,0 +1,14 @@
+package model.deck;
+
+/**
+ * Card is the supertype for model.deck.Weapon, model.deck.Tile and model.deck.PowerUp.
+ */
+
+public abstract class Card {
+
+    /**
+     * Constructor for model.deck.Card.
+     */
+    protected Card () {
+    }
+}
