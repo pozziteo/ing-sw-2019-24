dab597870fd176774785407ad14a98ca4e12a43a
Matteo <matteo.pozzi0397@gmail.com>
2019-03-30 17:03:15 +0100

    Add Ammo enumeration, which lists the three available "colors" of ammo
    Add attributes and methods to WeaponType class about the cost in ammos to reload and grab a weapon from the game board

---
+++ src/main/java/model/deck/Ammo.java
@@ -0,0 +1,43 @@
+package model.deck;
+
+/**
+ * Ammo is the enumeration class which defines the three different kinds of ammo.
+ */
+
+public enum Ammo {
+
+    RED_AMMO("Red"),
+    BLUE_AMMO("Blue"),
+    YELLOW_AMMO("Yellow");
+
+    /**
+     * color is the attribute which defines the kind of the ammo
+     */
+    private String color;
+
+    /**
+     * Constructor which initialize the ammo's color using the setter method.
+     *
+     * @param color which is the ammo's color
+     */
+    Ammo(String color) {
+        this.setColor(color);
+    }
+
+    /** Setter to initialize the color of the ammo.
+     *
+     * @param color which is the ammo's color (red, yellow or blue)
+     */
+    private void setColor(String color) {
+        this.color = color;
+    }
+
+    /**
+     * Getter to obtain the color of an ammo
+     *
+     * @return the ammo's color
+     */
+    public String getColor() {
+        return color;
+    }
+}
--- src/main/java/model/deck/WeaponType.java
+++ src/main/java/model/deck/WeaponType.java
@@ -1,5 +1,9 @@
 package model.deck;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * WeaponType is the enumeration class which lists all the weapon types in the game.
  *
@@ -8,27 +12,27 @@ package model.deck;
 
 public enum WeaponType {
 
-    LOCK_RIFLE("Lock Rifle"),
-    ELECTROSCYTHE("Electroscythe"),
-    MACHINE_GUN("Machine Gun"),
-    TRACTOR_BEAM("Tractor Beam"),
-    THOR("T.H.O.R."),
-    VORTEX_CANNON("Vortex Cannon"),
-    PLASMA_GUN("Plasma Gun"),
-    FURNACE("Furnace"),
-    HEATSEEKER("Heatseeker"),
-    WHISPER("Whisper"),
-    HELLION("Hellion"),
-    FLAMETHROWER("Flamethrower"),
-    ZX_2("ZX-2"),
-    GRENADE_LAUNCHER("Grenade Launcher"),
-    SHOTGUN("Shotgun"),
-    ROCKET_LAUNCHER("Rocket Launcher"),
-    POWER_GLOVE("Power Glove"),
-    RAILGUN("Railgun"),
-    SHOCKWAVE("Shockwave"),
-    CYBERBLADE("Cyberblade"),
-    SLEDGEHAMMER("Sledgehammer");
+    LOCK_RIFLE("Lock Rifle", Ammo.BLUE_AMMO, Ammo.BLUE_AMMO),
+    ELECTROSCYTHE("Electroscythe", Ammo.BLUE_AMMO),
+    MACHINE_GUN("Machine Gun", Ammo.BLUE_AMMO, Ammo.RED_AMMO),
+    TRACTOR_BEAM("Tractor Beam", Ammo.BLUE_AMMO),
+    THOR("T.H.O.R.", Ammo.BLUE_AMMO, Ammo.RED_AMMO),
+    VORTEX_CANNON("Vortex Cannon", Ammo.RED_AMMO, Ammo.BLUE_AMMO),
+    PLASMA_GUN("Plasma Gun", Ammo.BLUE_AMMO, Ammo.YELLOW_AMMO),
+    FURNACE("Furnace", Ammo.RED_AMMO, Ammo.BLUE_AMMO),
+    HEATSEEKER("Heatseeker", Ammo.RED_AMMO, Ammo.RED_AMMO, Ammo.YELLOW_AMMO),
+    WHISPER("Whisper", Ammo.BLUE_AMMO, Ammo.BLUE_AMMO, Ammo.YELLOW_AMMO),
+    HELLION("Hellion", Ammo.RED_AMMO, Ammo.YELLOW_AMMO),
+    FLAMETHROWER("Flamethrower", Ammo.RED_AMMO),
+    ZX_2("ZX-2", Ammo.YELLOW_AMMO, Ammo.RED_AMMO),
+    GRENADE_LAUNCHER("Grenade Launcher", Ammo.RED_AMMO),
+    SHOTGUN("Shotgun", Ammo.YELLOW_AMMO, Ammo.YELLOW_AMMO),
+    ROCKET_LAUNCHER("Rocket Launcher", Ammo.RED_AMMO, Ammo.RED_AMMO),
+    POWER_GLOVE("Power Glove", Ammo.YELLOW_AMMO, Ammo.BLUE_AMMO),
+    RAILGUN("Railgun", Ammo.YELLOW_AMMO, Ammo.YELLOW_AMMO, Ammo.BLUE_AMMO),
+    SHOCKWAVE("Shockwave", Ammo.YELLOW_AMMO),
+    CYBERBLADE("Cyberblade", Ammo.YELLOW_AMMO, Ammo.RED_AMMO),
+    SLEDGEHAMMER("Sledgehammer", Ammo.YELLOW_AMMO);
 
     /**
      * description is the name of the weapon (add some nice description for each weapon?)
@@ -36,12 +40,20 @@ public enum WeaponType {
     private String description;
 
     /**
-     * Constructor of WeaponType sets the description of each weapon.
-     *
-     * @param desc which is the description of the weapon passed to the setter method
+     * reloadingAmmo is the cost in ammo to reload a specific weapon.
      */
-    WeaponType(String desc) {
+    private List<Ammo> reloadingAmmo;
+
+
+    /**
+     * Constructor which initialize the description of the weapon and its cost in ammo for reloading.
+     *
+     * @param desc which is the name of the weapon.
+     * @param ammo which is the cost in ammo for reloading the weapon.
+     */
+    WeaponType(String desc, Ammo... ammo) {
         setDescription(desc);
+        setReloadingAmmo(ammo);
     }
 
     /**
@@ -54,6 +66,16 @@ public enum WeaponType {
     }
 
     /**
+     * Setter to initialize the cost in ammo to reload a weapon
+     *
+     * @param ammo which is the cost to reload the weapon
+     */
+    private void setReloadingAmmo(Ammo... ammo) {
+        reloadingAmmo = new ArrayList<Ammo>();
+        reloadingAmmo.addAll(Arrays.asList(ammo));
+    }
+
+    /**
      * Getter to obtain the description of a weapon.
      *
      * @return the name of the weapon used.
@@ -61,4 +83,29 @@ public enum WeaponType {
     public String getDescription() {
         return description;
     }
+
+    /**
+     * Getter to obtain the cost in ammo to reload a weapon
+     *
+     * @return the cost to reload the weapon
+     */
+    public List<Ammo> getReloadingAmmo() {
+        return reloadingAmmo;
+    }
+
+    /**
+     * Getter to obtain the cost in ammo to grab a weapon from the game board,
+     * which is the reload cost except the "top" ammo represented on the weapon's card, that
+     * is the first ammo memorized in the reloadingAmmo attribute.
+     *
+     * @return the cost to grab a weapon from the game board, that is the reloadingAmmo attribute
+     *          except its first element.
+     */
+    public List<Ammo> getGrabbingCost() {
+        ArrayList<Ammo> grabbingCost = new ArrayList<Ammo>(reloadingAmmo);
+        grabbingCost.remove(0);
+        return grabbingCost;
+    }
 }
+
+
