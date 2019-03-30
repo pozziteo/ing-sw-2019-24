c232eb931d2842327de26d769522a4d27b2c9e9b
sarasacco <sarasaccoo@gmail.com>
2019-03-30 19:03:09 +0100

    add methods for player and board

--- src/main/java/model/player/Board.java
+++ src/main/java/model/player/Board.java
@@ -1,11 +1,35 @@
 package model.player;
 
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
+
+/**
+ * Board is the class where every player has their
+ */
 
 public class Board {
-    private String damageTaken;
+    private String[12] damageTaken;
+    private String[3] receivedMarks;
     private ArrayList<Weapon> unloadedWeapons;
     private ArrayList<Ammo> ownedAmmo;
+    //private Action actionCard; ?
+    private int[5] pointsForKill;
+
+    public void gotHit(int damageAmount, Player inflictedByPlayer) { //add condition to check if damageTaken array is full
+        for (int i = 0; i < damageAmount; i++) {
+            this.damageTaken.push(inflictedByPlayer.playerID);
+        }
+    }
+
+    public void gotMarked(int amount, String markedByPlayer) {
+        try {
+            for (int i = 0; i < amount; i++)
+                this.receivedMarks.push(markedByPlayer);
+        } catch (Exception exception) {
+            System.out.println("Max amount of marks exceeded");
+        }
+    }
+
 
     public void addAmmo(Tile t) {
         this.ownedAmmo.add(t);
