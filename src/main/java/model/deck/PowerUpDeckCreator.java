6c8e7f1141dfb8d4eb8c216b9c3475b67c75e32d
Luca Pomè <luca.pome@mail.polimi.it>
2019-04-01 00:00:33 +0200

    first prototype of powerupdeckcreator

--- src/main/java/model/deck/PowerUpDeckCreator.java
+++ src/main/java/model/deck/PowerUpDeckCreator.java
@@ -1,4 +1,32 @@
 package model.deck;
 
-public class PowerUpDeckCreator {
+public class PowerUpDeckCreator extends DeckCreator {
+
+    /**
+     * this class calls the superclass constructor
+     */
+    protected PowerUpDeckCreator(){
+        super();
+    }
+
+    /**
+     * This method create a new shuffled PowerUpDeck
+     * return a PowerUpDeck
+     */
+    public PowerUpDeck createDeck(){
+
+        PowerUpDeck powerUpDeck = new PowerUpDeck();
+
+
+        for (PowerUpType type : PowerUpType.values()){
+            PowerUp powerUp = new PowerUp(type);
+            powerUpDeck.addCard(powerUp);
+        }
+
+        powerUpDeck.deckShuffle();
+
+        return powerUpDeck;
+
+    }
+
 }
