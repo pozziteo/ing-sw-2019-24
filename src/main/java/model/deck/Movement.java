dc0ad26ef0da328510413cd879a905b16ddc7157
Luca Pomè <luca.pome@mail.polimi.it>
2019-04-02 19:18:41 +0200

    fix

--- src/main/java/model/deck/Movement.java
+++ src/main/java/model/deck/Movement.java
@@ -13,19 +13,97 @@ public class Movement {
     private static final int SHORT=1;
     private static final int LONG=2;
 
-    public Square moveToMe (Player attacker, Player victim, int move){
 
-        return victim.getPosition();
+    /**
+     * This method changes the position of the victim. the attacker and the victim can't stay
+     * in the same square before the method is called
+     * @param attacker is the player who attracts the victim
+     * @param victim is the player who receive the movement
+     * @param move is the number of square the victim passes through (0 to 2)
+     */
+    public void moveToMe (Player attacker, Player victim, int move){
+
+        if(attacker.getPosition().getY()==victim.getPosition().getY()) {
+            if (attacker.getPosition().getX() > victim.getPosition().getX()) {
+                int x = victim.getPosition().getX();
+                victim.getPosition().setX(x + move);
+
+            }
+            if (attacker.getPosition().getX() < victim.getPosition().getX()) {
+                int x = victim.getPosition().getX();
+                victim.getPosition().setX(x - move);
+
+            }
+        }
+
+        if(attacker.getPosition().getX()==victim.getPosition().getX()) {
+            if (attacker.getPosition().getY() > victim.getPosition().getY()) {
+                int y = victim.getPosition().getY();
+                victim.getPosition().setY(y + move);
+
+            }
+            if (attacker.getPosition().getY() < victim.getPosition().getY()) {
+                int y = victim.getPosition().getY();
+                victim.getPosition().setY(y - move);
+
+            }
+        }
     }
 
-    public Square moveFromMe (Player attacker, Player victim, int move){
+    /**
+     * This method changes the position of the victim.
+     * @param attacker is the player who pushes away the victim
+     * @param victim is the player who receives the movement
+     * @param move is the number of square the victim passes through (0 to 2)
+     */
+    public void moveFromMe (Player attacker, Player victim, int move){
 
-        return victim.getPosition();
+        if(attacker.getPosition().getY()==victim.getPosition().getY()) {
+            if (attacker.getPosition().getX() > victim.getPosition().getX()) {
+                int x = victim.getPosition().getX();
+                victim.getPosition().setX(x - move);
+
+            }
+            if (attacker.getPosition().getX() < victim.getPosition().getX()) {
+                int x = victim.getPosition().getX();
+                victim.getPosition().setX(x + move);
+
+            }
+        }
+
+        if(attacker.getPosition().getX()==victim.getPosition().getX()) {
+            if (attacker.getPosition().getY() > victim.getPosition().getY()) {
+                int y = victim.getPosition().getY();
+                victim.getPosition().setY(y - move);
+
+            }
+            if (attacker.getPosition().getY() < victim.getPosition().getY()) {
+                int y = victim.getPosition().getY();
+                victim.getPosition().setY(y + move);
+
+            }
+        }
+
+        if ((attacker.getPosition().getY()==victim.getPosition().getY()) &&(attacker.getPosition().getX()==victim.getPosition().getX())){
+
+            //the attacker chooses the direction where to move away the victim (maybe controller?)
+
+            /*int x = victim.getPosition().getX();
+            victim.getPosition().setX(x - move);
+            victim.getPosition().setX(x + move);
+
+            int y = victim.getPosition().getY();
+            victim.getPosition().setY(y - move);
+            victim.getPosition().setY(y + move);
+            */
+        }
+
     }
 
-    public Square moveToSquare (Player victim, int move, Square toSquare){
+    public void moveToSquare (Player victim, int move, Square toSquare){
 
-        return victim.getPosition();
+        //the attacker chooses the square where to move the victim (maybe controller?)
+        //only few weapon can do it
     }
 
 
