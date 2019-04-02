f058cd1096bf81a8292d24e7f0f5dee6b70ca046
Luca Pomè <luca.pome@mail.polimi.it>
2019-04-02 15:32:59 +0200

    add attack class

--- src/main/java/model/deck/Attack.java
+++ src/main/java/model/deck/Attack.java
@@ -1,4 +1,29 @@
 package model.deck;
 
+import model.player.Player;
+import model.player.Board;
+
 public class Attack {
+
+    private int damage;
+    private int marks;
+    private Player attacker;
+    private Player victim;
+
+
+    /**
+     * @param attacker is the ID of the player who gives the damage
+     * @param victim is the ID of the player who receives the damage
+     * @param damage is the number of damage
+     */
+    public void giveDamage(Player attacker, Player victim, int damage) {
+        for (int i = 0; i < damage; i++) {
+            victim.getBoard().getDamage().add(attacker.getPlayerColor());
+
+        }
+
+    }
+
+
+
 }
