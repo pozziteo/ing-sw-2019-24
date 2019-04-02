5c343ea5fad2f6be6f93e347ca0ec03372f22399
Matteo <matteo.pozzi0397@gmail.com>
2019-04-02 17:52:56 +0200

    Add TilesDeck and TilesDeckCreator classes

---
+++ src/main/java/model/deck/TilesDeck.java
@@ -0,0 +1,14 @@
+package model.deck;
+
+/**
+ * TilesDeck is the subclass that represents a model.deck.Deck of Tile cards
+ */
+
+public class TilesDeck extends Deck {
+    /**
+     * Calls the superclass constructor
+     */
+    protected TilesDeck () {
+        super();
+    }
+}
