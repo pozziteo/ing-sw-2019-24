0ca51d0928023ea437d550d8c4252c48787eee72
Luca Pomè <luca.pome@mail.polimi.it>
2019-04-02 16:05:10 +0200

    fix

--- src/main/java/model/deck/Attack.java
+++ src/main/java/model/deck/Attack.java
@@ -1,17 +1,51 @@
 package model.deck;
 
-import model.player.Player;
-import model.player.Board;
+import model.player.*;
 
 public class Attack {
-
     private int damage;
     private int marks;
     private Player attacker;
     private Player victim;
 
+    public Attack(int d, int m, Player a, Player b){
+        this.damage = d;
+        this.marks = m;
+        this.attacker = a;
+        this.victim = b;
+    }
 
     /**
+     * Getter for the weapon's damage
+     * @return damage
+     */
+    public int getDamageWeapon(){return this.damage;}
+
+
+    /**
+     * Getter for the marks given by the weapon
+     * @return marks
+     */
+    public int getMarksWeapon() {return this.marks;}
+
+
+    /**
+     * Getter for the player's color
+     * @return attacker
+     */
+    public Player getAttacker() {return this.attacker;}
+
+
+    /**
+     * Getter for the victim' color
+     * @return victim
+     */
+    public Player getVictim() {return this.victim;}
+
+
+
+    /**
+     * This method adds damages to the victim
      * @param attacker is the ID of the player who gives the damage
      * @param victim is the ID of the player who receives the damage
      * @param damage is the number of damage
@@ -24,6 +58,9 @@ public class Attack {
 
     }
 
+    public void giveMarks() {
+        attacker.giveMark(marks, victim);
+    }
 
 
 }
