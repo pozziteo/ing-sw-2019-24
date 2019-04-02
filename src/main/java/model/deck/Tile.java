e31533ded46f3865d2845e114c16cbac23796a71
Matteo <matteo.pozzi0397@gmail.com>
2019-04-02 16:16:34 +0200

    Add Enum TileFormat with all the available formats of tiles and add attributes to Tile class

--- src/main/java/model/deck/Tile.java
+++ src/main/java/model/deck/Tile.java
@@ -2,4 +2,13 @@ package model.deck;
 
 public class Tile extends Card {
 
+    private TileFormat format;
+
+    public Tile(TileFormat format) {
+        this.format = format;
+    }
+
+    public String getTileContent() {
+        return format.getDescription();
+    }
 }
