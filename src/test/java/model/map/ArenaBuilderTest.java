9b349ca267e18824015f4e11e60464e254fa47bd
Matteo <matteo.pozzi0397@gmail.com>
2019-04-12 18:52:16 +0200

    Add test to check different kind of squares are used properly (i.e. unable to grab weapons from NormalSquares and unable to grab tiles from SpawnPoints)

--- src/test/java/model/map/ArenaBuilderTest.java
+++ src/test/java/model/map/ArenaBuilderTest.java
@@ -1,5 +1,7 @@
 package model.map;
 
+import model.deck.Tile;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
@@ -8,15 +10,35 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ArenaBuilderTest {
 
-    @Test
-    void createMapTest() {
+    private static Map map;
+
+    @BeforeAll
+    static void createMap() {
         try {
-            Map map = new ArenaBuilder().createMap();
-            assertEquals("red", map.getSquare(6).getSquareColor());
-            assertThrows(IndexOutOfBoundsException.class, () -> map.getSquare(map.getDimension()));
+        map = new ArenaBuilder().createMap();
         } catch (FileNotFoundException exc) {
             System.err.println("Error: Invalid map name selected");
             exc.printStackTrace();
         }
+    }
+
+    @Test
+    void createMapTest() {
+        assertEquals("red", map.getSquare(6).getSquareColor());
+        assertThrows(IndexOutOfBoundsException.class, () -> map.getSquare(map.getDimension()));
+    }
+
+    @Test
+    void checkSpawnPoint() {
+        assertTrue(map.getSquare(2) instanceof SpawnPoint);
+        assertTrue(map.getSquare(9) instanceof NormalSquare);
+    }
+
+    @Test
+    void checkUseOfSquares() {
+        assertThrows(ClassCastException.class, () -> { Square square = map.getSquare(2);
+            Tile tile = ((NormalSquare) square).getPlacedTile(); });
+        assertThrows(ClassCastException.class, () -> { Square square = map.getSquare(0);
+            ((SpawnPoint) square).getWeapons(); } );
     }
 }
