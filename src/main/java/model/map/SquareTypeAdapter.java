7cee2f1bd6f2b62c53836f54ea14b9580613f734
Matteo <matteo.pozzi0397@gmail.com>
2019-04-12 17:58:14 +0200

    Reduced Cyclomatic and Cognitive Complexity of SquareTypeAdapter class

--- src/main/java/model/map/SquareTypeAdapter.java
+++ src/main/java/model/map/SquareTypeAdapter.java
@@ -13,49 +13,46 @@ public class SquareTypeAdapter extends TypeAdapter<Square> {
 
     }
 
+    //TODO add JavaDoc
     @Override
     public Square read(JsonReader reader) throws IOException {
 
-        JsonToken token = reader.peek();
         Square square;
         int id = 0;
         String room = null;
         boolean sp = false;
         List<Integer> links = null;
 
-        if (token.equals(JsonToken.BEGIN_OBJECT)) {
-            reader.beginObject();
-            while (!reader.peek().equals(JsonToken.END_OBJECT)) {
-                if (reader.peek().equals(JsonToken.NAME)) {
-                    switch (reader.nextName()) {
-                        case "squareId":
-                            id = reader.nextInt();
-                            break;
-                        case "room":
-                                room = reader.nextString();
-                            break;
-                        case "spawn":
-                            sp = reader.nextBoolean();
-                            break;
-                        case "links":
-                            links = new ArrayList<Integer>();
-                            reader.beginArray();
-                            while (reader.hasNext()) {
-                                int link = reader.nextInt();
-                                links.add(link);
-                            }
-                            reader.endArray();
-                            break;
-                        default: reader.skipValue();
-                            break;
+        reader.beginObject();
+        while (reader.peek().equals(JsonToken.NAME)) {
+            switch (reader.nextName()) {
+                case "squareId":
+                    id = reader.nextInt();
+                    break;
+                case "room":
+                    room = reader.nextString();
+                    break;
+                case "spawn":
+                    sp = reader.nextBoolean();
+                    break;
+                case "links":
+                    links = new ArrayList<>();
+                    reader.beginArray();
+                    while (reader.hasNext()) {
+                        int link = reader.nextInt();
+                        links.add(link);
                     }
-                }
-            } reader.endObject();
+                    reader.endArray();
+                    break;
+                default:
+                    reader.skipValue();
+            }
         }
         if (sp)
             square = new SpawnPoint(id, room, links);
         else
             square = new NormalSquare(id, room, links);
+        reader.endObject();
         return square;
     }
 }
