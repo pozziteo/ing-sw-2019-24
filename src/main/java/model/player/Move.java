f25e3418a91b33d25ae458b7fe5a4007bc727209
Matteo <matteo.pozzi0397@gmail.com>
2019-04-13 17:47:31 +0200

    Corrected method to evaluate and validate valid moves of players. Add test to check valid moves are executed and invalid ones are not

--- src/main/java/model/player/Move.java
+++ src/main/java/model/player/Move.java
@@ -3,40 +3,55 @@ package model.player;
 import model.map.*;
 import model.map.Map;
 
-import java.io.FileNotFoundException;
 import java.util.*;
 
+//TODO add JavaDoc
 public class Move implements Action {
 
-    @Override
-    public String getActionInfo() {
-        return Action.super.getActionInfo() + "With this action you can move through up to 3 squares";
-    }
+    private List<Integer> paths;
 
-    public void findPaths(Player player) {
-        try {
-            System.out.println("You can move into squares:\n");
-            Map map = new ArenaBuilder().createMap();
-            Square position = player.getPosition();
-            List<Integer> toVisit = new ArrayList<>();
-            toVisit.add(position.getSquareId());
-            List<Integer> visited = new ArrayList<>();
-            for (int moves = 1; moves <= 3; moves++) {
+    public Move(Player player) {
+        System.out.println("You can move into squares:\n");
+        Map map = player.getGame().getArena();
+        Square position = player.getPosition();
+        List<Integer> toVisit = new ArrayList<>();
+        toVisit.add(position.getSquareId());
+        List<Integer> visited = new ArrayList<>();
+        for (int moves = 1; moves <= 3; moves++) {
+            System.out.print("[");
+            int currentNodes = toVisit.size();
+            while (currentNodes > 0) {
                 position = map.getSquare(toVisit.remove(0));
-                System.out.print("[");
                 for (int squareId : position.getLinks()) {
                     if (!visited.contains(squareId) && squareId != player.getPosition().getSquareId()) {
-                        System.out.print(" " + squareId);
+                        System.out.print(" (" + squareId/4 + ", " + squareId%4 + ") ");
                         visited.add(squareId);
                         toVisit.add(squareId);
                     }
                 }
-                System.out.print(" ] (" + moves + " movements)\n");
+                currentNodes--;
             }
-        } catch (FileNotFoundException exc) {
-            System.err.println("Error: Invalid map name selected");
-            exc.printStackTrace();
+            System.out.println(" ] (" + moves + " movements)");
         }
+        this.paths = visited;
+    }
+
+    public List<Integer> getPaths() {
+        return this.paths;
+    }
+
+    public Square takeMove(Player player, int xSquare, int ySquare) {
+        int squareId = xSquare*4 + ySquare;
+        if (paths.contains(squareId))
+            player.setPosition(player.getGame().getArena().getSquare(squareId));
+        else player.setPosition(player.getPosition());
+
+        return player.getPosition();
+    }
+
+    @Override
+    public String getActionInfo() {
+        return Action.super.getActionInfo() + "With this action you can move through up to 3 squares";
     }
 
     @Override
