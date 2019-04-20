479b1cdc7357808537b2e2e93b13682003aa36e7
Luca Pomè <luca.pome@mail.polimi.it>
2019-04-20 12:11:32 +0200

    fix server, initial client

---
+++ src/main/java/controller/Client.java
@@ -0,0 +1,74 @@
+package controller;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+
+    private int port;
+    private String serverAddress;
+
+    /**
+     * constructor of client socket
+     * @param port represents the socket port of the server
+     * @param serverAddress represents the IP address (on the same machine it might be called localhost)
+     */
+    public Client(String serverAddress, int port){
+        this.port = port;
+        this.serverAddress = serverAddress;
+    }
+
+    /**
+     * This Method starts the client
+     */
+    public void startClient() throws NullPointerException, IOException {
+
+
+        Socket socket = new Socket("localhost", 6666);
+        OutputStream output = socket.getOutputStream();
+        PrintWriter writer = new PrintWriter(output, true);
+        InputStream in = socket.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        //Scanner input = new Scanner(socket.getInputStream());
+        Scanner stdin = new Scanner(System.in);
+        try {
+
+
+
+            while(true) {
+
+                String inputText = stdin.nextLine();
+                writer.println(inputText);
+                writer.flush();
+
+
+                String text = reader.readLine();
+                System.out.println(text);
+
+            }
+
+
+        } catch (IOException e){
+            System.err.println(e.getMessage());
+        }
+    }
+
+
+    /**
+     * Main method
+     * @param args
+     * @throws NullPointerException
+     * @throws IOException
+     */
+    public static void main(String[] args) throws NullPointerException, IOException {
+
+        Client client = new Client("localhost", 6666);
+        System.out.println("The client is now running. " +
+                "Connecting to the server...");
+        System.out.println("Connection established");
+        client.startClient();
+        System.out.println("Connection lost");
+
+    }
+}
--- src/main/java/controller/Server.java
+++ src/main/java/controller/Server.java
@@ -3,6 +3,7 @@ package controller;
 import java.io.IOException;
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class Server {
 
@@ -18,35 +19,67 @@ public class Server {
     }
 
     /**
-     * This method handles every new client that joins the server.
-     * This method allows bidirectional communication between client and server
+     * This method starts the server;
+     * It also handles every new client that joins the server
+     * and allows bidirectional communication between client and server
      * @throws IOException
      */
     public void startServer() throws IOException {
 
-       try {
-           ss = new ServerSocket(port);
+        int i = 1;
 
+        try {
+           ss = new ServerSocket(port);
            System.out.println("the server is ready on port: " + port);
 
-           while (true) {
+
+
+           while (i<5) {
+
                Socket s = ss.accept();
+               Scanner input = new Scanner(s.getInputStream());
+               PrintWriter output = new PrintWriter(s.getOutputStream());
+               Scanner stdin = new Scanner(System.in);
+               String sInput= stdin.nextLine();
+               System.out.println("A new client is here: Client" +i);
 
-               System.out.println("A new client is here.");
+               String text = input.nextLine();
+               if (text.equals("end")){break;}
 
-               InputStream input = s.getInputStream();
-               OutputStream output = s.getOutputStream();
+               System.out.println("From Client"+i +": " + text);
 
-               PrintWriter writer = new PrintWriter(output, true);
+
+
+               output.println("From Server:" + sInput);
+               output.flush();
+
+
+             /*  PrintWriter writer = new PrintWriter(output, true);
+               System.out.println(input);
+               writer.println("welcome Client" +i);
                writer.println("Sent by The SERVER!");
 
+                 InputStream in = s.getInputStream();
+               BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+               String text = reader.readLine();
+               System.out.println(text);*/
+
+               i = i+1;
+               /*input.close();
+               output.close();
+               s.close();*/
            }
+
+
+
+
        } catch (IOException e){
            System.out.println(e.getMessage());
            e.printStackTrace();
-       } finally {
-           ss.close();
        }
+
+        ss.close();
+
     }
 
 
