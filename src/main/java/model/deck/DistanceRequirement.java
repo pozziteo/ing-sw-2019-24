b677a856862cb3fb4295fa77e915485158cf821f
Matteo <matteo.pozzi0397@gmail.com>
2019-04-24 23:52:49 +0200

    Implemented method to find targets int distance requirement

--- src/main/java/model/deck/DistanceRequirement.java
+++ src/main/java/model/deck/DistanceRequirement.java
@@ -1,5 +1,6 @@
 package model.deck;
 
+import model.player.Action;
 import model.player.Player;
 
 import java.util.ArrayList;
@@ -15,14 +16,76 @@ public class DistanceRequirement implements WeaponEffectRequirement {
         this.maxDistance = maxDistance;
     }
 
+
+    /**
+     * Method to find the possible targets that the attacker may hit with a certain effect.
+     * In this implementation they are calculated by finding the squares that match the constraints
+     * of distance and evaluating the players on those squares,
+     * also applying the visibility bond - which is always implied
+     *
+     * @param attacker is the player who wants to attack
+     * @return a list of players that may be hit with an effect under this requirement
+     */
     @Override
     public List<Player> findTargets(Player attacker) {
         List<Player> targets = new ArrayList<>();
         List<Player> players = new ArrayList<>(attacker.getGame().getPlayers());
         players.remove(attacker);
 
-        
+        List<Integer> validSquares = findValidSquares(attacker);
 
-        return null;
+        for (Player player : players)
+            if (checkPosition(player, validSquares) && attacker.canSee(player))
+                targets.add(player);
+
+        return targets;
+    }
+
+
+    /**
+     * Method to obtain the squares that satisfy the distance requirement from the attacker. Three different cases can
+     * occur:
+     * 1 - minDistance is 0 -> a valid value of maxDistance is supposed (unless this would be a visibility requirement
+     *      and not a distance one), the squares are the paths from the attacker for maxDistance movements
+     *
+     * 2 - minDistance is not 0 and maxDistance is 0 -> there is only a minimum distance from the attacker, the valid
+     *      squares are all the squares except the ones under the minimum distance
+     *
+     * 3 - none of the previous -> there are both a minimum and a maximum distance, the valid squares are the paths from
+     *      the attacker for maxDistance movements except the ones under the minimum distance
+     *
+     * @param attacker is the player who the squares are calculated from
+     * @return all the valid squares that satisfy the constraints
+     */
+    private List<Integer> findValidSquares(Player attacker) {
+
+        List<Integer> validSquares = new ArrayList<>();
+
+        if (minDistance == 0) {
+            validSquares = Action.findPaths(attacker, maxDistance);
+        }
+        else if (maxDistance == 0) {
+            for (int id=0; id < attacker.getGame().getArena().getDimension(); id++)
+                validSquares.add(id);
+            List<Integer> tooNear = Action.findPaths(attacker, minDistance-1);
+            validSquares.removeAll(tooNear);
+        }
+        else {
+            validSquares = Action.findPaths(attacker, maxDistance);
+            List<Integer> tooNear = Action.findPaths(attacker, minDistance-1);
+            validSquares.removeAll(tooNear);
+        }
+
+        return validSquares;
+    }
+
+    private boolean checkPosition(Player target, List<Integer> validSquares) {
+
+        for (Integer squareId : validSquares)
+            if (target.getPosition().getSquareId() == squareId)
+                return true;
+        return false;
+
     }
 }
+
