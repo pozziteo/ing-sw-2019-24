4caccda6783b4356b25d2c1b4f83be1ae96c9f1d
Luca Pomè <luca.pome@mail.polimi.it>
2019-04-26 12:19:42 +0200

    added methods and tips

--- src/main/java/network/rmi/client/RmiClient.java
+++ src/main/java/network/rmi/client/RmiClient.java
@@ -1,5 +1,26 @@
 package network.rmi.client;
 
+import network.rmi.commoninterface.CommonInterface;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class RmiClient {
+
+    private RmiClient(){}
+
+    public void startRmiClient() {
+
+        try {
+            Registry registry = LocateRegistry.getRegistry();
+            CommonInterface stub = (CommonInterface) registry.lookup("CommonInterface");
+
+            /* DataType var = stub.METHOD(); //here you call the methods you need
+            System.out.println("what you called: " +var);
+            */
+        }catch (Exception e){
+            System.err.println("Client exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
 
 }
--- src/main/java/network/rmi/server/RmiServer.java
+++ src/main/java/network/rmi/server/RmiServer.java
@@ -1,17 +1,56 @@
 package network.rmi.server;
 
-public class RmiServer {
+import network.rmi.commoninterface.CommonInterface;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class RmiServer implements CommonInterface{
     private boolean running;
 
+    /**
+     * Constructor that creates a not-running RmiServer
+     */
     public RmiServer() {
         this.running = false;
     }
 
+    /**
+     * this Method runs the RmiServer on default port 1099
+     * It stops when boolean "running" becomes false
+     */
     public void startServer() {
         running = true;
+        while(running) {
+            try {
+
+                RmiServer server = new RmiServer();
+                CommonInterface stub = (CommonInterface) UnicastRemoteObject.exportObject(server, 0);
+
+                //bind the remote object's stub in the registry
+                Registry registry = LocateRegistry.getRegistry();
+                registry.bind("CommonInterface", stub);
+                System.err.println("RmiServer ready on default port 1099");
+
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+                running = false;
+            }
+        }
     }
 
+    /**
+     * this Method that shuts down the server
+     */
+    public void shutDownRmiServer(){
+        running = false;
+    }
 
+    /**
+     * getter Method of the running state of the server
+     * @return a boolean that indicates if the server is running or not
+     */
     public boolean getRunning() {
         return this.running;
     }
