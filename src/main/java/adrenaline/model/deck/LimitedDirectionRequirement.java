13604dfcee298dddcd418c59aec56336f865569d
Matteo <matteo.pozzi0397@gmail.com>
2019-05-07 13:35:03 +0200

    Write JSON file for Flame Thrower weapon and its particular requirement

---
+++ src/main/java/adrenaline/model/deck/LimitedDirectionRequirement.java
@@ -0,0 +1,36 @@
+package adrenaline.model.deck;
+
+import adrenaline.model.player.Action;
+import adrenaline.model.player.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LimitedDirectionRequirement extends DirectionRequirement {
+
+    private int maxDistance;
+
+    protected LimitedDirectionRequirement(int maxDistance) {
+        this.maxDistance = maxDistance;
+    }
+
+    @Override
+    public List<Player> findTargets(Player attacker) {
+        List<Player> limitedTargets = new ArrayList<>();
+        List<Player> directionTargets = super.findTargets(attacker);
+        List<Integer> validPositions = Action.findPaths(attacker, maxDistance);
+        for (Player target : directionTargets) {
+            if (checkPosition(target, validPositions))
+                limitedTargets.add(target);
+        }
+        return limitedTargets;
+    }
+
+    private boolean checkPosition(Player target, List<Integer> validSquares) {
+        for (int squareId : validSquares) {
+            if (target.getPosition().getSquareId() == squareId)
+                return true;
+        }
+        return false;
+    }
+}
