7db08c6a1d7116e13c8bcca19c44c07c3f09e0bc
Matteo <matteo.pozzi0397@gmail.com>
2019-07-04 11:35:05 +0200

    Add RMI JavaDoc

--- src/main/java/adrenaline/network/rmi/RmiClientCallbackInterface.java
+++ src/main/java/adrenaline/network/rmi/RmiClientCallbackInterface.java
@@ -5,8 +5,23 @@ import adrenaline.data.data_for_client.DataForClient;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+/**
+ * Interface that declares methods callable by the RMI server on a RMI client, it is used by the server to identify
+ * each client
+ */
 public interface RmiClientCallbackInterface extends Remote {
 
+    /**
+     * Invoked when server is sending a pack of data to the client.
+     * @param data is the pack of data to be sent
+     * @throws RemoteException if an error occurs in the communication on the net
+     */
     void notifyChanges(DataForClient data) throws RemoteException;
+
+    /**
+     * Invoked to check if the client is still online and listening
+     * @return true if the client is still online, false otherwise
+     * @throws RemoteException if an error occurs in the communication on the net
+     */
     boolean ping() throws RemoteException;
 }
